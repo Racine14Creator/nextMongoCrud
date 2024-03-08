7f35e0a5de82741c4b4cf7408c226195f17b9cdb
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-03-08 07:11:18 +0200

    add: config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    // omitted for brevity...
+                    { key: "Access-Control-Allow-Origin", value: "https://todo-omega-olive-97.vercel.app" },
+                ]
+            },
+            {
+                source: "/api/todo",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "false" },
+                    { key: "Access-Control-Allow-Origin", value: "https://todo-omega-olive-97.vercel.app" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+                ]
+            }
+        ]
+    }
+};
 
 export default nextConfig;
