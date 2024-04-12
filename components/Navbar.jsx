b3a2c23217f2e1c7f28debefafa6af17df622aef
racine14creator <bisimwagrace596@gmail.com>
2024-04-12 08:52:19 +0200

    add: Kinde auth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
 const Navbar = () => {
     return (
@@ -6,6 +7,8 @@ const Navbar = () => {
             <Link href={"/"} className="text-white font-bold">Racine14 Todo</Link>
             <ul>
                 <li><Link href={"/addTodo"} className="bg-white p-2">Add Todo</Link></li>
+                <li><LoginLink className="bg-white p-2">Log In</LoginLink></li>
+                <li><RegisterLink className="bg-white p-2">Register</RegisterLink></li>
             </ul>
         </nav>
     )
