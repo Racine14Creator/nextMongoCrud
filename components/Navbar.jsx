b07260aed594bb39fdbbb7a8935daffa28d97a72
racine14creator <bisimwagrace596@gmail.com>
2024-04-12 09:30:17 +0200

    add: dynamic navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,23 @@
 import Link from "next/link"
 import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
+const navLinks = [
+    {name: "Dashboard", href: "/"},
+    {name: "Profile", href: "/"},
+]
 const Navbar = () => {
     return (
         <nav className="flex justify-between items-center bg-slate-700 px-8 py-3">
             <Link href={"/"} className="text-white font-bold">Racine14 Todo</Link>
-            <ul>
-                <li><Link href={"/addTodo"} className="bg-white p-2">Add Todo</Link></li>
+            <ul className="flex gap-x-2">
+                {navLinks.map((item, index)=>(
+                    <li key={index}>
+                        <Link href={item.href} className="bg-white p-2">
+                        {item.name}
+                    </Link>
+                    </li>
+                ))}
+                
                 <li><LoginLink className="bg-white p-2">Log In</LoginLink></li>
                 <li><RegisterLink className="bg-white p-2">Register</RegisterLink></li>
             </ul>
